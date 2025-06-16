7020136851591371a669bbb3636361f01a7648f3
abhinav030903 <abhinavupadhyay2004@gmail.com>
2025-06-16 23:19:16 +0530

    Update Patterns.cpp

--- Patterns.cpp
+++ Patterns.cpp
@@ -62,9 +62,23 @@ void pattern7(int N) {
         cout << endl;
     }
 }
+void pattern8(int N) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            cout <<" ";
+        }
+        for(int j = 0; j< 2 * N - (2 * i + 1); j++) {
+            cout<<"*";
+        }
+        for (int j = 0; j < i; j++) {
+            cout <<" ";
+        }
+        cout << endl;
+    }
+}
 int main() {
     int N;
     cin >> N;
-    pattern7(N);
+    pattern8(N);
     return 0;
 }
